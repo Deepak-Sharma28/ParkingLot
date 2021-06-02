55a4f69342eb49379de09dbee9f0e7a036a2078a
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-06-02 19:19:13 +0530

    made some changes

--- Parking.js
+++ Parking.js
@@ -3,54 +3,54 @@ const fs = require("fs");
 
 
 
-// for creating a lot
+// // for creating a lot
 
-const createLots = (capacity = Prompt("Enter number of slots")) => {
-    var Obj = {
-        "Slots": []
-    };
-    for (let counter = 0; counter < Number(capacity); counter++) {
-        Obj.Slots.push("Empty");
-    }
-    fs.writeFileSync("input.json", JSON.stringify(Obj));
-};
-
-
-// createLots();
-
-
-
-//for Parking a car
-
-
-// const carPark = (carNumber = Prompt("Enter the car number")) => {
-//     const slots = JSON.parse(fs.readFileSync("input.json", "utf-8"));
-
-
-//     var index = -1;
-//     if (typeof(slots.Slots.find(slot => {
-//             index++;
-//             return slot === "Empty";
-//         })) !== "undefined") {
-
-//         slots.Slots[index] = {
-//             car: {
-//                 "carNumber": carNumber,
-//                 "parkingTime": new Date(),
-//                 "leavingTime": ""
-//             }
-//         };
-//         fs.writeFileSync("input.json", JSON.stringify(slots));
-//         console.log(slots);
-
-//     } else {
-//         return "do not have empty parking slot here";
+// const createLots = (capacity = Prompt("Enter number of slots")) => {
+//     var Obj = {
+//         "Slots": []
+//     };
+//     for (let counter = 0; counter < Number(capacity); counter++) {
+//         Obj.Slots.push("Empty");
 //     }
+//     console.log(`Created parking lot with ${counter+1} slots`);
+//     fs.writeFileSync("input.json", JSON.stringify(Obj));
 // };
+
+
+// // createLots();
+
+
+
+// //for Parking a car
+
+
+const carPark = (carNumber = Prompt("Enter the car number")) => {
+    const slots = JSON.parse(fs.readFileSync("input.json", "utf-8"));
+    var index = -1;
+    if (typeof(slots.Slots.find(slot => {
+            index++;
+            return slot === "Empty";
+        })) !== "undefined") {
+        var current = new Date();
+
+        slots.Slots[index] = {
+            car: {
+                "carNumber": carNumber,
+                "parkingTime": `${current.getHours()} : ${current.getMinutes()} : ${current.getSeconds()}`,
+            }
+        };
+        fs.writeFileSync("input.json", JSON.stringify(slots));
+        // console.log(slots);
+        console.log(`Allocated slot number: ${index+1}`);
+
+    } else {
+        console.log("do not have empty parking slot here");
+    }
+};
 
 // carPark();
 
-//for leaving the parking
+// //for leaving the parking
 
 
 const leavePark = (carNumber = Prompt("Enter the car Number")) => {
@@ -61,12 +61,17 @@ const leavePark = (carNumber = Prompt("Enter the car Number")) => {
         return typeof(Slots.Slots[index]) === "object" && Slots.Slots[index].car.carNumber === carNumber;
     });
     if (index >= 0) {
-        console.log(`car number ${Slots.Slots[index].car.carNumber} has leaved it spent ${new Date().getHours()-Number(Slots.Slots[0].car.parkingTime.slice(11, 13)) }`);
+
+        var time = new Date().getHours() - Number(Slots.Slots[0].car.parkingTime.slice(0, 3).trim());
+
+        console.log(`${Slots.Slots[index].car.carNumber} number with Slot Number${index++} is free with Charge ${(time) <= 2 ? 20 : time * 10 } $`);
+
         Slots.Slots[index] = "Leaved";
+
         fs.writeFileSync("input.json", JSON.stringify(Slots));
     } else {
+
         console.log("Please check your entered carNumber");
     }
 };
-
-leavePark();
+// leavePark();
